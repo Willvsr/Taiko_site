cabcf8ee86458f2f313db3fa40ac03cd21ddde3b
Willvsr <williamvsrodrigues@gmail.com>
2025-08-21 10:12:53 -0300

    Atualização do repositório

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  styleUrls: ['./home.component.css'],
   standalone: true,
   imports: [CommonModule]
 })
