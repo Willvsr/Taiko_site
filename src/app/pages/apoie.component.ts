8b1d7985efff3e026efd36883b70fe56fa6e58f4
Willvsr <williamvsrodrigues@gmail.com>
2025-10-28 14:05:50 -0300

    adicionado as logos das redes sociais

--- src/app/pages/apoie.component.ts
+++ src/app/pages/apoie.component.ts
@@ -7,7 +7,7 @@ import { CommonModule } from '@angular/common';
   selector: 'app-apoie',
   template: `
     <div class="apoie-section">
-      <h2>Apoie o Ishindaiko</h2>
+      <h2>Apoie o Kouryuu Daiko</h2>
       <div class="apoie-content">
         <section>
           <h3>Como Apoiar</h3>
